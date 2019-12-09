31bfc351a1efa8fc5613260e03ada06973052824
melia miller <meliajm@gmail.com>
2019-12-09 22:00:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,14 @@ def countdown(starting_count)
   end
   "HAPPY NEW YEAR!"
 end
+
+
+
+def countdown_with_sleep(starting_count)
+  while starting_count > 0 do
+    puts "#{starting_count} SECOND(S)!"
+    sleep(1)
+    starting_count -= 1 
+  end
+  "HAPPY NEW YEAR!"
+end 

a3fdc0cbe0818170f4bc9acaa1f53ee6106c2cf2
melia miller <meliajm@gmail.com>
2019-12-09 21:58:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ require 'pry'
 def countdown(starting_count)
   
   while starting_count > 0 do 
-    puts "#{starting_count} SECOND(S)"
+    puts "#{starting_count} SECOND(S)!"
     starting_count -= 1 
     # binding.pry
   end

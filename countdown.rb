0cfb03c4e9c389705ac13903010c46a604cc35d9
melia miller <meliajm@gmail.com>
2019-12-09 21:58:02 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(starting_count)
   while starting_count > 0 do 
     puts "#{starting_count} SECOND(S)"
     starting_count -= 1 
-    binding.pry
+    # binding.pry
   end
   "HAPPY NEW YEAR!"
 end
